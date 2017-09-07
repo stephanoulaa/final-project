aecf45a813d13f957e3970a07fead6f25c6bbfd8
stephanoula <stephprodromos@gmail.com>
2017-09-07 09:48:11 -0600

    third

---
+++ login.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#signup-form').on('submit', function(event){
+        event.preventDefault()
+        var signupInfo = {
+            username : $('#signup-form .username').val(),
+            password : $('#signup-form .password').val(),
+        }
+        $.post('/signup', signupInfo, function(data){
+            console.log(data)
+            window.location.href="/dashboard"
+        })
+    })
+
+    $('#login-form').on('submit', function(event){
+        event.preventDefault()
+        var signupInfo = {
+            username : $('#login-form .username').val(),
+            password : $('#login-form .password').val(),
+        }
+        $.post('/login', signupInfo, function(data){
+            console.log(data)
+            window.location.href="/dashboard"
+        })
+    })
+
+
+})
